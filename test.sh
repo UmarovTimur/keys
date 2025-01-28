83879ef19b35771f6381d0816ab8d557aea00c57
Timur <84568033+UmarovTimur@users.noreply.github.com>
2025-01-28 11:34:38 +0500

    Update test.sh
    no check certificate

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-yt-dlp --force-ipv4 --cookies cookies.txt -o - "https://www.youtube.com/watch?v=_bmo-5RGXB4" | ffmpeg -re -i pipe:0 -c:v libx264 -preset veryfast -b:v 2500k -maxrate 2500k -bufsize 5000k -c:a aac -b:a 128k -f flv "rtmp://your.streaming.server/live2/26ex-m1pc-7tjq-detp-1f1x"
+yt-dlp --no-check-certificate --cookies cookies.txt -o - "https://www.youtube.com/watch?v=_bmo-5RGXB4" | ffmpeg -re -i pipe:0 -c:v libx264 -preset veryfast -b:v 2500k -maxrate 2500k -bufsize 5000k -c:a aac -b:a 128k -f flv "rtmp://your.streaming.server/live2/26ex-m1pc-7tjq-detp-1f1x"
