5498eb52bfdc59e17d3aeca1746e0163eaa5a282
Timur <84568033+UmarovTimur@users.noreply.github.com>
2025-01-28 10:40:11 +0500

    Update youtubeTr.sh
    without playlist.txt

--- youtubeTr.sh
+++ youtubeTr.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-PLAYLIST="playlist.txt"
-STREAM_URL="rtmp://a.rtmp.youtube.com/live2/26ex-m1pc-7tjq-detp-1f1x" # Замените на URL вашего RTMP-сервера
+# URL вашего YouTube плейлиста
+PLAYLIST_URL="https://www.youtube.com/playlist?list=PLgnSUXTkFZ22TW4h2g_3QBnjKJVsHr_Yd"
 
+# RTMP-сервер (замените на ваш адрес)
+STREAM_URL="rtmp://your.streaming.server/live2/26ex-m1pc-7tjq-detp-1f1x"
+
+# Получаем список видео из плейлиста и начинаем трансляцию
 while true; do
-    while read -r VIDEO_URL; do
+    # Получаем список видео из плейлиста и передаем его в цикл
+    yt-dlp -j --flat-playlist "$PLAYLIST_URL" | jq -r '.url' | while read -r VIDEO_URL; do
         echo "Streaming: $VIDEO_URL"
+        # Запускаем видео через ffmpeg, отправляя его на RTMP сервер
         yt-dlp -o - "$VIDEO_URL" | ffmpeg -re -i pipe:0 -c:v libx264 -preset veryfast -b:v 2500k -maxrate 2500k -bufsize 5000k \
             -c:a aac -b:a 128k -f flv "$STREAM_URL"
-    done < "$PLAYLIST"
+    done
 done
+
