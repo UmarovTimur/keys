e5fadac5c45a31fdba81f5035b145f17648968bb
Timur <84568033+UmarovTimur@users.noreply.github.com>
2025-01-28 11:49:44 +0500

    Update test.sh
    all

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-yt-dlp --cookies-from-browser chrome -o - "https://www.youtube.com/watch?v=_bmo-5RGXB4" | ffmpeg -re -i -c:v libx264 -preset veryfast -b:v 2500k -maxrate 2500k -bufsize 5000k -c:a aac -b:a 128k -f flv "rtmp://your.streaming.server/live2/26ex-m1pc-7tjq-detp-1f1x"
+yt-dlp --extractor-args "youtube:player_client=android" --cookies cookies.txt -o - "https://www.youtube.com/watch?v=_bmo-5RGXB4" | ffmpeg -re -i -c:v libx264 -preset veryfast -b:v 2500k -maxrate 2500k -bufsize 5000k -c:a aac -b:a 128k -f flv "rtmp://your.streaming.server/live2/26ex-m1pc-7tjq-detp-1f1x"
